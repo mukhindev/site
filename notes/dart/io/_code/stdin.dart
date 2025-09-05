ea0ccd373ab73077c3e21b1b1b482189bc8c0b28
Sergey Mukhin <mukhin.dev@gmail.com>
2025-09-05 15:33:58 +0200

    Контент выведен как git submodule

--- notes/dart/io/_code/stdin.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:io';
-
-main() {
-  String? line = stdin.readLineSync();
-  print('Было введено "$line"');
-}
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,18 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const notes = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "notes" }),
+  loader: glob({
+    // Заметки должны в папке notes (кроме черновиков, оканчиваются на .draft.md)
+    pattern: ["**/notes/**/*.md", "!**/*.draft.md"],
+    base: "external",
+    generateId: ({ entry, data }) => {
+      // Забираем для id страницы название файла
+      const match = entry.match(/\/notes\/(.+?)(?:\.[^/.]+)?$/);
+      const path = match![1];
+      // В path можно передать (через метаданные md файла) стабильный путь независимый от реального положения
+      return (data.path as string | undefined) ?? path;
+    },
+  }),
   schema: z.object({
     date: z.coerce.date(),
     updateDate: z.coerce.date().optional(),
